24f1cd63d03ecdbc407f36e0a274101008d1f684
Emyjakarta <ukonud4@gmail.com>
2024-03-20 12:07:38 +0100

    Solved dictionary representation

--- console.py
+++ console.py
@@ -179,7 +179,80 @@ class HBNBCommand(cmd.Cmd):
                 instance_id = method_arg.split('(')[1][:-1]
                 self.do_destroy(f"{class_name} {instance_id}")
                 return
-            
+            elif re.match(r"(\w+)\.(\w+)\((.*)\)", line):
+                print("MARRY")
+                get_regex = list(re.match(r"(\w+)\.(\w+)\((.*)\)", line).groups())
+                print("{} {}".format("get_regex =", get_regex))
+                # Get rid of empty lines in the returned pattern
+                if get_regex[-1] == "":
+                    get_regex.pop()
+
+                if get_regex:
+                    if len(get_regex) >= 2:
+                        class_name, user_cmd = get_regex[0], get_regex[1]
+                        line = f"{user_cmd} {class_name}"
+
+                        try:
+                            obj_dict = re.findall(r"\{.*?\}", get_regex[2])[0]
+
+                            if user_cmd == "update" and obj_dict:
+                                obj_dict = eval(obj_dict)
+                                print("{} {}".format("obj_dict after eval=", obj_dict))
+                                instance_id = shlex.split(get_regex[2])[0].replace(",", "")
+                                print("{} {}".format("instance_id after =", instance_id))
+                                line += f" {instance_id} {obj_dict}"
+                                print("{} {}".format("line at end of try block b4 onecmd =", line))
+                                print("{} {}".format("line at end of try block after strip b4 onecmd =", line.strip()))
+                                self.onecmd(line.strip())
+                                #self.do_update(f"{class_name} {instance_id} {obj_dict}")
+                                return
+                                #return line
+                        except IndexError:
+                            #print("** instance id missing **")
+                            pass
+
+                        # Try and preserve a list if it exists
+                        if len(get_regex) >= 3:
+                            list_data = re.findall(r"\[.*\]", get_regex[2])
+                            if list_data:
+                                get_regex[2] = get_regex[2].replace(str(list_data[0]), "")
+
+                        try:
+                            extra_args = shlex.split(get_regex[2])
+                            line += " "
+                            line += " ".join(extra_args).replace(",", "")
+                            line += f" {list_data or ''}"
+                        except (ValueError, IndexError):
+                            pass
+            self.onecmd(line.strip())
+            return #line.strip()
+        #     elif re.match(r"update\((.*)\)", method_arg):
+        #         # Extracting instance id and attribute information from method_arg
+        #         instance_id, attribute_info = re.match(r"update\((.*)\)", method_arg).group(1).split(',', 1)
+        #         attribute_info = attribute_info.strip()
+
+        #         if attribute_info.startswith('{') and attribute_info.endswith('}'):
+        #             print("{} {}".format("instance_id =", instance_id))
+        #             print("{} {}".format("attribute_info =", attribute_info))
+        #             try:
+        #                 # Parse the dictionary representation into a Python dictionary
+        #                 attr_dict = eval(attribute_info)
+        #                 print("{} {}".format("attr_dict =", attr_dict))
+        #             except (NameError, SyntaxError):
+        #                 print("** invalid dictionary representation **")
+        #                 return
+
+        #             # Update instance attributes with the dictionary values
+        #             self.do_update(f"{class_name} {instance_id} {attribute_info}")
+        #         else:
+        #             # Update with attribute name and value
+        #             attribute_name, attribute_value = attribute_info.split(',', 1)
+        #             attribute_name = attribute_name.strip()
+        #             attribute_value = attribute_value.strip()
+        #             self.do_update(f"{class_name} {instance_id} {attribute_name} {attribute_value}")
+        #         return
+
+        # super().default(line)
             # elif method_arg.startswith('update(') and method_arg.endswith(')'):
             #     # Extract instance ID and attribute information using regular expressions
             #     match = re.match(r'update\("(.*?)",\s*({.*})\)', method_arg)
@@ -223,37 +296,37 @@ class HBNBCommand(cmd.Cmd):
             #         return
 
     # super().default(line)
-            elif method_arg.startswith('update(') and method_arg.endswith(')'):
-                # Extracting instance id and attribute information from method_arg
-                instance_id, attribute_info = method_arg.split('(')[1][:-1].split(',', 1)
-                attribute_info = attribute_info.strip()
-                if attribute_info.startswith('{') and attribute_info.endswith('}'):
-                    print("{} {}".format("instance_id =", instance_id))
-                    print("{} {}".format("attribute_info =", attribute_info))
-                    # Update with dictionary representation
-                    print("Hello")
-                    try:
-                        # Parse the dictionary representation into a Python dictionary
-                        attr_dict = eval(attribute_info)
-                        #print("{} {}".format("args[2] =", args[2]))
-                        print("{} {}".format("attr_dict =", attr_dict))
-                    except (NameError, SyntaxError):
-                        print("** invalid dictionary representation **")
-                        return
+        #     elif method_arg.startswith('update(') and method_arg.endswith(')'):
+        #         # Extracting instance id and attribute information from method_arg
+        #         instance_id, attribute_info = method_arg.split('(')[1][:-1].split(',', 1)
+        #         attribute_info = attribute_info.strip()
+        #         if attribute_info.startswith('{') and attribute_info.endswith('}'):
+        #             print("{} {}".format("instance_id =", instance_id))
+        #             print("{} {}".format("attribute_info =", attribute_info))
+        #             # Update with dictionary representation
+        #             print("Hello")
+        #             try:
+        #                 # Parse the dictionary representation into a Python dictionary
+        #                 attr_dict = eval(attribute_info)
+        #                 #print("{} {}".format("args[2] =", args[2]))
+        #                 print("{} {}".format("attr_dict =", attr_dict))
+        #             except (NameError, SyntaxError):
+        #                 print("** invalid dictionary representation **")
+        #                 return
 
-                    #Update instance attributes with the dictionary values
-                    #for attr_name, attr_value in attr_dict.items():
-                        #setattr(obj, attr_name, attr_value)
+        #             #Update instance attributes with the dictionary values
+        #             #for attr_name, attr_value in attr_dict.items():
+        #                 #setattr(obj, attr_name, attr_value)
 
-                    self.do_update(f"{class_name} {instance_id} {attribute_info}")
-                else:
-                    # Update with attribute name and value
-                    attribute_name, attribute_value = attribute_info.split(',', 1)
-                    attribute_name = attribute_name.strip()
-                    attribute_value = attribute_value.strip()
-                    self.do_update(f"{class_name} {instance_id} {attribute_name} {attribute_value}")
-                return
-        super().default(line)
+        #             self.do_update(f"{class_name} {instance_id} {attribute_info}")
+        #         else:
+        #             # Update with attribute name and value
+        #             attribute_name, attribute_value = attribute_info.split(',', 1)
+        #             attribute_name = attribute_name.strip()
+        #             attribute_value = attribute_value.strip()
+        #             self.do_update(f"{class_name} {instance_id} {attribute_name} {attribute_value}")
+        #         return
+        # super().default(line)
 
    
 
@@ -280,8 +353,24 @@ class HBNBCommand(cmd.Cmd):
        
     def do_update(self, arg):
         """Updates an instance based on the class name and id."""
-        print("{} {}".format("arg =", arg))
+        print("{} {}".format("arg in do_update =", arg))
+        obj_dict = re.findall(r"\{.*?\}", arg)
+        print("{} {}".format("obj_dict in do_update =", obj_dict))
+        print("{} {}".format("arg[2] in do_update =", arg[2]))
+        #print("{} {}".format("arg in do_update(list) =", list(arg)))
+        #get_regex = list(re.match(r"(\w+)\.(\w+)\((.*)\)", arg).groups())
+        #pattern = r"^(\w+)\.update\(\"(\w+)\",\s*(\{.*\})\)$"
+        #pattern = r"^(\w+)\s+(\w+)\s+(\{.*\})$"
+        pattern = r"(.*)"
+        match = re.match(pattern, arg)
+        print("{} {}".format("match in do_update =", match))
+        instance_id_match = re.search(r'\b[\da-f]{8}(-[\da-f]{4}){3}-[\da-f]{12}\b', match.group(1))
+        print("{} {}".format("instance_id_match =", instance_id_match))
+        if not instance_id_match:
+            print("** instance id missing **")
+            return
         args = shlex.split(arg)
+        print("{} {}".format("args in do_update =", args))
         models = storage.all()
 
         if not args:
@@ -292,11 +381,14 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
 
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
+        # if len(args) < 2:
+        # #f arg[1] not in shlex.split(get_regex[2])[0].replace(",", ""):
+        #     print("** instance id missing **")
+        #     return
 
         key = "{}.{}".format(args[0], args[1])
+        print("{} {}".format("key =", key))
+        print("{} {}".format("storage.all() =", storage.all()))
         if key not in storage.all():
         #if key not in models:
             print("** no instance found **")
@@ -309,29 +401,33 @@ class HBNBCommand(cmd.Cmd):
 
         # Extract and parse the dictionary representation using ast.literal_eval
         print("{} {}".format("args[2:] =", args[2:]))
-        dict_repr = "{'Five': 5, 'Six': '6'}"
-        dict_obj = ast.literal_eval(dict_repr)
-        print("{} {}".format("dict_repr (hardcode) =", dict_repr))
+        #dict_repr = "{'Five': 5, 'Six': '6'}"
+        #dict_obj = ast.literal_eval(dict_repr)
+        #print("{} {}".format("dict_repr (hardcode) =", dict_repr))
         #dict_repr = " ".join(args[2:])
-        dict_repr = args[-1]
-        if dict_repr.startswith('{') and dict_repr.endswith('}'):
+        #dict_repr = args[-1]
+        #print("{} {}".format("dict_repr (soft) =", dict_repr))
+        if obj_dict:
+        #if dict_repr.startswith('{') and dict_repr.endswith('}'):
             print("World")
             #dict_repr = " ".join(args[2:])
-            print("{} {}".format("dict_repr =", dict_repr))
-            print("{} {}".format("dict_repr[0] =", dict_repr[0]))
-            print("{} {}".format("dict_repr[1] =", dict_repr[1]))
-            print("{} {}".format("dict_repr[2] =", dict_repr[2]))
-            print("{} {}".format("dict_repr[5] =", dict_repr[5]))
-            print("{} {}".format("args[2] =", args[2]))
+            # print("{} {}".format("dict_repr =", dict_repr))
+            # print("{} {}".format("dict_repr[0] =", dict_repr[0]))
+            # print("{} {}".format("dict_repr[1] =", dict_repr[1]))
+            # print("{} {}".format("dict_repr[2] =", dict_repr[2]))
+            # print("{} {}".format("dict_repr[5] =", dict_repr[5]))
+            # print("{} {}".format("args[2] =", args[2]))
             # attr_dict = ast.literal_eval(dict_repr)
             # print("{} {}".format("attr_dict1 =", attr_dict))
-            print("{} {}".format("dict_repr before try block =", dict_repr))
+            print("{} {}".format("obj_dict before try block =", obj_dict))
+            dictionary_string = obj_dict[0].strip("'")
             try:
                 print("Hurray")
-                attr_dict = ast.literal_eval(dict_repr)
+                dict_repr = ast.literal_eval(dictionary_string)
+                # dict_repr = eval(obj_dict)
                 #attr_dict = eval(dict_repr)
                 #print("{} {}".format("args[2] =", args[2]))
-                print("{} {}".format("attr_dict =", attr_dict))
+                #print("{} {}".format("obj_dict =", obj_dict))
                 # for attr_name, attr_value in attr_dict.items():
                 #     setattr(obj, attr_name, attr_value)
                 print("{} {}".format("dict_repr before exception =", dict_repr))
@@ -342,7 +438,7 @@ class HBNBCommand(cmd.Cmd):
         
 
             #Update instance attributes with the dictionary values
-            for attr_name, attr_value in attr_dict.items():
+            for attr_name, attr_value in dict_repr.items():
                 setattr(obj, attr_name, attr_value)
         else:
             # If the third argument is an attribute name and fourth argument is a value
