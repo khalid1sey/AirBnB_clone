d96747aa2a6d9feba7b0595edee382791c0df6c4
Emyjakarta <ukonud4@gmail.com>
2024-03-25 10:50:28 +0100

    Resolved pycodestyle errors

--- models/review.py
+++ models/review.py
@@ -16,12 +16,3 @@ class Review(BaseModel):
     place_id = ""
     user_id = ""
     text = ""
-
-    def __init__(self, *args, **kwargs):
-            """Initialize Review instance."""
-            super().__init__(*args, **kwargs)
-
-    def save(self):
-        """Update the updated_at attribute and save."""
-        self.updated_at = datetime.now()
-        super().save()  # Call save method of the BaseModel class
